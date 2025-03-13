42937dd253f481c981c794c34b25aa073c13493b
Elliotte Rusty Harold <elharo@ibiblio.org>
2025-03-13 06:30:39 -0400

    dataclass

--- src/decache/cache_entry.py
+++ src/decache/cache_entry.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
 
+@dataclass
 class CacheEntry:
-    def __init__(self, url: str, media_type: str):
-        self.url = url
-        self.media_type = media_type
+
+    url: str
+    media_type: str
 
--- tests/test_cache_entry.py
+++ tests/test_cache_entry.py
@@ -1,7 +1,11 @@
 from decache import cache_entry
 
 
-def test_something():
+def test_cache_entry():
     entry = cache_entry.CacheEntry(url="https://www.example.org/foo.jpg", media_type="image/jpg")
     assert entry.url == "https://www.example.org/foo.jpg"
     assert entry.media_type == "image/jpg"
+
+
+def test_cache_map():
+    pass
